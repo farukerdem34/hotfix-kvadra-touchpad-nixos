f4b12c181c595fc4c9b073266a3d7cad95f454e2
Alexander Pevzner <pzz@apevzner.com>
2025-01-29 12:39:23 +0300

    Refactoring/cosmetic

--- module.c
+++ module.c
@@ -18,9 +18,14 @@
 #include <linux/pci.h>
 #include <linux/pci_ids.h>
 
-#define KVADRA_PROC_NAME        "kvadra"
-#define KVARDA_IRQ_NUM          27
-#define KVARDA_IRQ_NAME         "kvadra"
+// Hardcoded parameters:
+//
+//   MODULE_NAME        - name, used for logging etc
+//   MODULE_PROCFS_NAME - name, used by /proc entry
+//   MODULE_IRQ_NAME    - name, listed in /proc/interrupts
+#define MODULE_NAME             "hotfix-kvadra-touchpad"
+#define MODULE_PROCFS_NAME      MODULE_NAME
+#define MODULE_IRQ_NAME         MODULE_NAME
 
 // i2c_controller contains information on each detected I2C controller.
 typedef struct {
@@ -28,6 +33,9 @@ typedef struct {
     atomic_t isr_count; // ISR invocation count
 } i2c_controller;
 
+// I2C_CONTROLLER_MAX defines size limits for the I2C controller list.
+//
+// Typical system contains 4-6 of such controllers, so 16 must be enough...
 #define I2C_CONTROLLER_MAX      16
 
 // i2c_controller_list contains list of detected I2C controllers.
@@ -35,14 +43,16 @@ typedef struct {
 static i2c_controller i2c_controller_list[I2C_CONTROLLER_MAX];
 static int            i2c_controller_count;
 
-// kvadra_detect_i2c_controllers detects I2C controllers in the
+// i2c_controller_detect detects I2C controllers in the
 // system and populates i2c_controller_list.
 //
 // Returns 0 on success, -errno on a error.
 static int
-kvadra_detect_i2c_controllers (void)
+i2c_controller_detect (void)
 {
     struct pci_dev *dev = pci_get_device(PCI_VENDOR_ID_INTEL, PCI_ANY_ID, NULL);
+
+    pr_info(MODULE_NAME ": I2C controllers detected:\n");
 
     while (dev != NULL) {
         switch (dev->device) {
@@ -51,7 +61,9 @@ kvadra_detect_i2c_controllers (void)
         case 0x51e9:
         case 0x51c5:
         case 0x51c6:
-            pr_info("PCI: %4.xx:%4.x IRQ %d\n", dev->vendor, dev->device, dev->irq);
+            pr_info("  PCI: %4.xx:%4.x IRQ %d\n",
+                dev->vendor, dev->device, dev->irq);
+
             i2c_controller_list[i2c_controller_count].irq = dev->irq;
             i2c_controller_count ++;
 
@@ -67,10 +79,10 @@ kvadra_detect_i2c_controllers (void)
     return 0;
 }
 
-// kvadra_isr is the interrupt service routine that this module installs
+// irq_handler is the interrupt service routine that this module installs
 // for each detected I2C controller.
 static irqreturn_t
-kvadra_isr (int, void *p)
+irq_handler (int, void *p)
 {
     i2c_controller *c = p;
     atomic_inc(&c->isr_count);
@@ -103,7 +115,7 @@ hook_interrupts (void)
         i2c_controller *c = &i2c_controller_list[i];
         int            rc;
 
-        rc = request_irq(c->irq, kvadra_isr, IRQF_SHARED, KVARDA_IRQ_NAME, c);
+        rc = request_irq(c->irq, irq_handler, IRQF_SHARED, MODULE_IRQ_NAME, c);
         if (rc < 0) {
             release_interrupts(i);
             return rc;
@@ -113,7 +125,7 @@ hook_interrupts (void)
     return 0;
 }
 
-// proc_read handles read from the /proc entry
+// proc_show handles read from the /proc entry
 static int
 proc_show(struct seq_file *m, void *v)
 {
@@ -127,25 +139,39 @@ proc_show(struct seq_file *m, void *v)
     return 0;
 }
 
-// kvadra_init implements module initialization.
-static int kvadra_init(void)
+// mod_init implements module initialization.
+static int mod_init(void)
 {
     struct proc_dir_entry *ent;
     int                   rc;
+    const char            *s;
 
-    pr_info("KVADRA hack driver loaded\n");
-    pr_info("Vendor:  %s", dmi_get_system_info(DMI_SYS_VENDOR));
-    pr_info("Product: %s", dmi_get_system_info(DMI_PRODUCT_NAME));
-    pr_info("Board:   %s", dmi_get_system_info(DMI_BOARD_NAME));
+    // Write log messages
+    pr_info(MODULE_NAME ": loaded\n");
+
+    s = dmi_get_system_info(DMI_SYS_VENDOR);
+    if (s != NULL) {
+        pr_info("  System Vendor:  %s", s);
+    }
+
+    s = dmi_get_system_info(DMI_PRODUCT_NAME);
+    if (s != NULL) {
+        pr_info("  Product name:,  %s", s);
+    }
+
+    s = dmi_get_system_info(DMI_BOARD_NAME);
+    if (s != NULL) {
+        pr_info("  Board Name:     %s", s);
+    }
 
     // Detect I2C controllers
-    rc = kvadra_detect_i2c_controllers();
+    rc = i2c_controller_detect();
     if (rc < 0) {
         return rc;
     }
 
     // Crete /proc entry
-    ent = proc_create_single(KVADRA_PROC_NAME, 0644, NULL, proc_show);
+    ent = proc_create_single(MODULE_PROCFS_NAME, 0644, NULL, proc_show);
     if (ent == NULL) {
     	return -ENOENT;
     }
@@ -153,27 +179,29 @@ static int kvadra_init(void)
     // Hook interrupts
     rc = hook_interrupts();
     if (rc < 0) {
-        remove_proc_entry(KVADRA_PROC_NAME, NULL);
+        remove_proc_entry(MODULE_PROCFS_NAME, NULL);
         return rc;
     }
+
+    pr_info(MODULE_NAME ": initialized\n");
 
     return 0;
 }
 
-// kvadra_init implements module cleanup.
-static void kvadra_exit(void)
+// mod_init implements module cleanup.
+static void mod_exit(void)
 {
     release_interrupts(i2c_controller_count);
-    remove_proc_entry(KVADRA_PROC_NAME, NULL);
-    pr_info("KVADRA hack driver removed\n");
+    remove_proc_entry(MODULE_PROCFS_NAME, NULL);
+    pr_info(MODULE_NAME ": removed\n");
 }
 
 // Module description
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Alexander Pevzner");
-MODULE_DESCRIPTION("KVADRA hack");
+MODULE_DESCRIPTION("Touchpad hotfix for KVADRA NAU LE14U and similar");
 
-module_init(kvadra_init);
-module_exit(kvadra_exit);
+module_init(mod_init);
+module_exit(mod_exit);
 
 // vim:ts=8:sw=4:et
